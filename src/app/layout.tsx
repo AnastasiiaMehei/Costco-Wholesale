0c68dfaaf9beb1f078227663b2cea6c5d059e027
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-10-07 21:44:28 +0300

    components, companies, loyout, dashboard, id,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
 
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -8,8 +10,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
